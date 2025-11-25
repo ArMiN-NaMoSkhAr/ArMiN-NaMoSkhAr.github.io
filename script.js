c18cd710ef0f27614cd3b21022a6682da5e28587
ArMiN-NaMoSkhAr <sediqmahbod@gmail.com>
2025-11-25 14:48:24 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll Reveal Animation
+const boxes = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+    boxes.forEach(box => {
+        const top = box.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            box.classList.add("show");
+        }
+    });
+});
