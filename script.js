15df66e237318478580765bd14d4097923943678
PEDAR-MORDEGAN-IR <sediqmahbod@gmail.com>
2025-11-27 01:38:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
 // ===========================
-//  Show Splash Screen ONLY Once
-// ===========================
-
-window.addEventListener("load", () => {
-
-    // آیا قبلاً اسپلش دیده شده؟
-    const splashSeen = localStorage.getItem("splashDone");
-
-    if (!splashSeen) {
-        // اسپلش را ۵ ثانیه نشان بده
-        setTimeout(() => {
-            const splash = document.getElementById("splash");
-            if (splash) splash.remove();
-        }, 5000);
-
-        // فقط یک بار ذخیره کن
-        localStorage.setItem("splashDone", "true");
-
-    } else {
-        // اگر اسپلش قبلاً دیده شده → فوری حذفش کن
-        const splash = document.getElementById("splash");
-        if (splash) splash.remove();
-    }
-});
-
-
-// ===========================
 //  Scroll Reveal Animation
 // ===========================
 
